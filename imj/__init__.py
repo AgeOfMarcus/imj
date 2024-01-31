e6837b3b4b28a0407d0ac4e7d21b1f88dfb019c0
Marcus Weinberger <marcusjw@duck.com>
2024-01-31 09:32:22 -0500

    added support for deletion

--- imj/__init__.py
+++ imj/__init__.py
@@ -1,33 +1,35 @@
 import requests
 
-BASE = 'https://img.marcusweinberger.repl.co'
+BASE = 'https://i.marcusj.org'
 
 class IMJ(object):
-    def __init__(self, img_id, base_url='https://img.marcusweinberger.repl.co',
-    disp_url='https://i.marcusj.tech'):
+    def __init__(self, img_id: str, key: str, base_url: str=BASE, disp_url: str=BASE):
         self.id = img_id
         self.base = base_url
         self.disp = disp_url
+        self.key = key
 
     @property
-    def viewer(self):
+    def viewer(self) -> str:
         return f"{self.disp}/view/{self.id}"
     
     @property
-    def url(self):
+    def url(self) -> str:
         return f"{self.disp}/image/{self.id}"
     
     def shorten(self):
         return requests.get(f"{self.base}/shorten/{self.id}").url.replace(self.base, self.disp)
     
+    def delete(self):
+        return requests.get(f"{self.base}/delete/{self.id}?key={self.key}").text
 
-def upload_file(fn):
+def upload_file(fn: str) -> IMJ:
     r = requests.post(f"{BASE}/upload", files={
         'image': open(fn, 'rb')
     })
     return IMJ(r.url.split('/')[-1])
 
-def upload(raw, fn='image.png', mimetype='image/png'):
+def upload(raw: bytes, fn: str='image.png', mimetype: str='image/png') -> IMJ:
     r = requests.post(f"{BASE}/upload", files={
         'image': (
             fn,
@@ -36,3 +38,6 @@ def upload(raw, fn='image.png', mimetype='image/png'):
         )
     })
     return IMJ(r.url.split('/')[-1])
+
+def delete(img_id: str, key: str) -> str:
+    return requests.get(f"{BASE}/delete/{img_id}?key={key}").text
--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 import setuptools
 
 setuptools.setup(name="imj",
-    version="1.0.2",
-    description="Python client for i.marcusj.tech",
+    version="1.1.0",
+    description="Python client for i.marcusj.org",
     long_description=open('README.md','r').read(),
     long_description_content_type="text/markdown",
     url="https://github.com/AgeOfMarcus/imj",
